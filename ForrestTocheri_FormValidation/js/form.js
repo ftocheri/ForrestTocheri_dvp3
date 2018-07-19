10b55973f0b55e03f6bae2cabad870ac661c82bc
Forrest Tocheri <fstocheri@student.fullsail.edu>
2018-07-18 19:30:20 -0600

    continued work on w3 assignment.
    Changed placeholders and disabled button

--- ForrestTocheri_FormValidation/js/form.js
+++ ForrestTocheri_FormValidation/js/form.js
@@ -2,3 +2,8 @@ document.querySelector('form h2').innerHTML = "<strong>Contact</strong>Me";
 var d = document.querySelectorAll('form p');
 d[0].innerHTML = "Let me know of any updates or errors that I have made while discussing these games. Also, if there is a topic you would like me to cover in the future.";
 document.body.style.backgroundImage = "url('https://wallpapertag.com/wallpaper/full/b/e/4/342028-game-background-2560x1440-for-windows.jpg')";
+d[1].innerHTML = "<label for='tag'>tag</label><input type='text' name='tag' id='tag' class='required' placeholder='Tag' />";
+d[3].innerHTML = "<label for='fgame'>Favorite Game</label><input type='text' name='fgame' id='phone' class='required' placeholder='Favorite Game' />";
+
+document.querySelector('form button').disabled = true;
+
