810df32812dccc9ae5e57140812fab10e4bcd7e4
Forrest Tocheri <fstocheri@student.fullsail.edu>
2018-07-18 18:54:50 -0600

    Added formValidation assignment

---
+++ ForrestTocheri_FormValidation/js/form.js
@@ -0,0 +1,4 @@
+document.querySelector('form h2').innerHTML = "<strong>Contact</strong>Me";
+var d = document.querySelectorAll('form p');
+d[0].innerHTML = "Let me know of any updates or errors that I have made while discussing these games. Also, if there is a topic you would like me to cover in the future.";
+document.body.style.backgroundImage = "url('https://wallpapertag.com/wallpaper/full/b/e/4/342028-game-background-2560x1440-for-windows.jpg')";
